d7f399c8d15fdc51f481768d7abf8b66d2002dd3
danielDumville <daniel.dumville@se.experis.com>
2021-08-23 14:45:27 +0200

    Added project structure and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingViewComponent } from './views/landing/landing-view/landing-view.component';
+import { TrainerViewComponent } from './views/trainer/trainer-view/trainer-view.component';
+import { CatalogueViewComponent } from './views/catalogue/catalogue-view/catalogue-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingViewComponent,
+    TrainerViewComponent,
+    CatalogueViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/views/catalogue/catalogue-view/catalogue-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogue-view',
+  templateUrl: './catalogue-view.component.html',
+  styleUrls: ['./catalogue-view.component.css']
+})
+export class CatalogueViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
