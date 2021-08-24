a6d771f52b3ac316442096b0ada436959d0dfd3b
danielDumville <daniel.dumville@se.experis.com>
2021-08-24 09:02:05 +0200

    Set routing in views

--- src/app/views/landing/landing-view/landing-view.component.ts
+++ src/app/views/landing/landing-view/landing-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
+import { Subscription } from 'rxjs';
+import { LandingServiceService } from 'src/app/services/landing-service.service';
 
 @Component({
   selector: 'app-landing-view',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-view.component.css']
 })
 export class LandingViewComponent implements OnInit {
+  subscription: Subscription;
+  showLandingView: boolean = true;
 
-  constructor() { }
+  
+  constructor(private landingService: LandingServiceService ,private router:Router) {
+    this.subscription = this.landingService.onToggle().subscribe(value => this.showLandingView = value)
+   }
 
   ngOnInit(): void {
+    this.toggleLandingView()
+    this.hasRoute("'/'")
   }
+  hasRoute(route: string){
+    return this.router.url === route;
 
+  }
+  toggleLandingView(){
+    this.landingService.toggleLandingView();
+  }
 }
